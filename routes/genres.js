4f1ec480ff57bc4e2a692d70ca8a9bc17f6b1bf0
lolaloka <mhmdazab4@gmaile.com>
2020-04-25 22:50:59 +0200

    creating a seprate file for routes handlebars

--- routes/genres.js
+++ routes/genres.js
@@ -1,89 +1,17 @@
-const { Genre, validate } = require("../models/geners.model");
 const express = require("express");
 const router = express.Router();
-router.get("/", async (req, res) => {
-  try {
-    const genres = await Genre.find().sort("name");
-    if (!genres) return res.status(400).send(" Some Error ");
-    res.send(genres);
-  } catch (error) {
-    console.log(error);
-  }
-});
-// Get specific genre
-router.get("/:id", async (req, res) => {
-  const genre = await Genre.findById(req.params.id);
-  if (!genre)
-    return res.status(404).send("The Genre with The Given Id Is not Found");
-  res.send(genre);
-});
-
-// Create New genres
-router.post("/", async (req, res) => {
-  const { error } = validate(req.body);
-  if (error) {
-    res.status(404).send(error.details[0].message);
-  }
-  let genre = new Genre({ name: req.body.name });
-  genre = await genre.save();
-  res.send(genre);
-});
-// Update an existing gener
-router.put("/:id", async (req, res) => {
-  try {
-    const { error } = validate(req.body);
-    if (error) return res.status(404).send(error.details[0].message);
-
-    const genre = await Genre.findById(req.params.id);
-    if (!genre)
-      res.status(400).send("The Genre with The Given Id Is not Found");
-    if (genre.name === req.body.name)
-      return res.status(400).send("it seems that its has the same name");
-    genre.set({
-      name: req.body.name
-    });
-    const SavedCoures = await genre.save();
-    res.send(SavedCoures);
-  } catch (err) {
-    console.log(err);
-    res.status(500).send("Failed To Connect");
-  }
-});
+const handbars = require("../controllers/handelbars");
+// Get All Movies
+router.get("/", handbars.GetAllGenres);
+// Get specifi cgenre
+router.get("/:id", handbars.Getspecificgenre);
+// Create New genre
+router.post("/", handbars.CreateNewgenre);
+// Update existing gener
+router.put("/:id", handbars.UpdateAgenre);
 // Delete an existing gener
-router.delete("/:id", async (req, res) => {
-  try {
-    const genre = await Genre.findByIdAndRemove(req.params.id);
-    if (!genre)
-      return res.status(404).send("The Genre with The Given Id Is not Found");
-    if (genre.length < 0)
-      return res.status(404).send("it seems that there is No Id");
-
-    res.send(genre);
-  } catch (error) {
-    console.log(error);
-  }
-});
+router.delete("/:id", handbars.DeleteAGenre);
 // Delete All
-router.delete("/", async (req, res) => {
-  // try {
-  //   const genre = await Genre.findByIdAndRemove(req.params.id);
-  //   if (!genre)
-  //     return res.status(404).send("The Genre with The Given Id Is not Found");
-  //   if (genre.length < 0)
-  //     return res.status(404).send("it seems that there is No Id");
-
-  //   res.send(genre);
-  // } catch (error) {
-  //   console.log(error);
-  // }
-  const ids = req.body;
-  let deletedMovie = Genre.find({ _id: { $in: ids } });
-  if (deletedMovie.length === 0)
-    return res.status(400).send("it seems that Not Movies to delete");
-  deletedMovie = deletedMovie.map(async movie => await movie.remove());
-
-  res.send(deletedMovie.map(id => deletedMovie.id));
-  // await Genre.findById(await (id)=>{})
-});
+router.delete("/", handbars.DeleteAllGenre);
 
 module.exports = router;
